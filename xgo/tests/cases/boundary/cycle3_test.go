ceb69686f657ebc25b2c3b01a5a313951471fdd1
wuyu <wuyu@yunify.com>
2024-06-22 23:32:07 +0800

    Fix typo: Rename world circular to cycle

---
+++ xgo/tests/cases/boundary/cycle3_test.go
@@ -0,0 +1,46 @@
+package boundary
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/yu31/protoc-plugin-json/xgo/tests/pb/pbboundary"
+)
+
+func Test_InlineMessageCycle3(t *testing.T) {
+	seed1 := &pbboundary.InlineMessageCycle3{
+		FString1: "s101",
+		FString2: "s102",
+		FMessage1: &pbboundary.InlineMessageCycle3_EmbedMessage1{
+			FString3: "s103",
+			FString4: "s104",
+			FMessage2: &pbboundary.InlineMessageCycle3_EmbedMessage2{
+				FString5: "s105",
+				FString6: "s106",
+				FMessage3: &pbboundary.InlineMessageCycle3_EmbedMessage1{
+					FString3:  "s107",
+					FString4:  "s108",
+					FMessage2: nil,
+				},
+			},
+		},
+	}
+
+	var (
+		err error
+		bb  []byte
+	)
+
+	t.Run("Marshal", func(t *testing.T) {
+		bb, err = seed1.MarshalJSON()
+		require.Nil(t, err)
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		dataNew := &pbboundary.InlineMessageCycle3{}
+		err = dataNew.UnmarshalJSON(bb)
+		require.Nil(t, err)
+		require.Equal(t, seed1, dataNew)
+	})
+}
---
+++ xgo/tests/cases/boundary/cycle4_test.go
@@ -0,0 +1,44 @@
+package boundary
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/yu31/protoc-plugin-json/xgo/tests/pb/pbboundary"
+)
+
+func Test_InlineMessageCycle4(t *testing.T) {
+	seed1 := &pbboundary.InlineMessageCycle4{
+		FString1: "s101",
+		FString2: "s102",
+		FMessage1: &pbboundary.InlineMessageCycle4_EmbedMessage1{
+			FString3: "s103",
+			FString4: "s104",
+			OneType2: &pbboundary.InlineMessageCycle4_EmbedMessage1_FMessage2{
+				FMessage2: &pbboundary.InlineMessageCycle4{
+					FString1:  "s105",
+					FString2:  "s106",
+					FMessage1: nil,
+				},
+			},
+		},
+	}
+
+	var (
+		err error
+		bb  []byte
+	)
+
+	t.Run("Marshal", func(t *testing.T) {
+		bb, err = seed1.MarshalJSON()
+		require.Nil(t, err)
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		dataNew := &pbboundary.InlineMessageCycle4{}
+		err = dataNew.UnmarshalJSON(bb)
+		require.Nil(t, err)
+		require.Equal(t, seed1, dataNew)
+	})
+}
